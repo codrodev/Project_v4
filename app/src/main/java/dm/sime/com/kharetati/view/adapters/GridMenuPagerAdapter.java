bca5720a96c769a760fb44b0f567027fb13c9391
satya <codrodev>
2020-03-13 18:13:23 +0530

    1. Viewpager OnDestroyItem method

--- app/src/main/java/dm/sime/com/kharetati/view/adapters/GridMenuPagerAdapter.java
+++ app/src/main/java/dm/sime/com/kharetati/view/adapters/GridMenuPagerAdapter.java
@@ -55,7 +55,7 @@ public class GridMenuPagerAdapter extends PagerAdapter {
 
     @Override
     public void destroyItem(ViewGroup container, int position, Object object) {
-       super.destroyItem((View) container, position, object);
+       //super.destroyItem((View) container, position, object);
     }
 
     @Override
